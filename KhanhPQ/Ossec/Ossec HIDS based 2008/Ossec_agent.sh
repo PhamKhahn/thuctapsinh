03930a897d7452d9ffcd562694137a8455df04a1
PhamKhahn <Pham.khanh.1.9.1996@gmail.com>
2020-03-07 16:31:25 +0700

    Update folder Ossec

---
+++ KhanhPQ/Ossec/Ossec HIDS based 2008/Ossec_agent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum install -y epel-release zlib-devel pcre2-devel make gcc mysql-devel postgresql-devel sqlite-devel
+yum install -y wget
+wget -q -O - https://updates.atomicorp.com/installers/atomic | sh
+yum install -y ossec-hids ossec-hids-agent
