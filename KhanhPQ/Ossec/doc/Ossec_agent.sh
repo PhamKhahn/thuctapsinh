2519ec5ca1711fd21f4a124f62d472d33b7b22a6
PhamKhahn <Pham.khanh.1.9.1996@gmail.com>
2020-03-10 17:48:24 +0700

    Update Ossec syscheck

--- KhanhPQ/Ossec/doc/Ossec_agent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum install -y epel-release zlib-devel pcre2-devel make gcc mysql-devel postgresql-devel sqlite-devel
-yum install -y wget
-wget -q -O - https://updates.atomicorp.com/installers/atomic | sh
-yum install -y ossec-hids ossec-hids-agent
